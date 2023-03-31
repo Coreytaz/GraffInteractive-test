780c096c3ca95b5e99012250292c584be8c7f918
Coreytaz <coreytaz321@gmail.com>
2023-03-31 17:18:15 +0500

    fix | add InputList

---
+++ src/entities/InputList/ui/index.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode, useEffect, useRef, useState } from 'react'
+import cn from 'clsx'
+import styles from './styles.module.scss'
+import { Icon } from '../../../shared/ui/Icon'
+
+const InputList: FC<{ action: ReactNode, value: String[] }> = ({ action, value }) => {
+    const [popup, setPopup] = useState(false)
+    const sortRef = useRef<HTMLDivElement>(null)
+
+    return (
+        <div className={styles.wrapperList}>
+            <label>
+                Label
+            </label>
+            <div className={cn(styles.inputList_dropdown, { [styles.active]: popup })} ref={sortRef}>
+                <div className={cn(styles.inputList_dropdown_header, { [styles.gradient]: popup })} onClick={() => setPopup(!popup)}>
+                    {value?.length > 0 ? <span>Выбрано {value?.length}</span> : <span>Любой</span>}{!popup ? <Icon.ChevronDown /> : <Icon.ChevronUp />}
+                </div>
+                {popup && <div className={styles.inputList_dropdown_content}>
+                    {action}
+                </div>}
+            </div>
+        </div>
+    )
+}
+
+export default InputList
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,9 @@
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import { Button } from "../../shared/ui/Button";
 import { CheckboxGroup } from "../../shared/ui/checkbox";
 import { Icon } from "../../shared/ui/Icon";
 import { RadioButtonGroup } from "../../shared/ui/radio";
+import { InputList } from "../../entities/InputList";
 
 const IndexPage = () => {
     const drinks = [
@@ -17,8 +18,6 @@ const IndexPage = () => {
     ];
     const [selectedValue, setSelectedValue] = useState<String>(drinks[0].label);
     const [selectedCheckbox, setSelectedCheckbox] = useState<String[]>(['Tea']);
-    console.log(selectedCheckbox)
-    console.log(selectedValue)
     return (
         <>
             <div style={{ margin: '20px' }}>
@@ -28,10 +27,13 @@ const IndexPage = () => {
                 <RadioButtonGroup options={drinks} onChange={setSelectedValue} />
             </div>
             <div style={{ margin: '20px' }}>
-                <CheckboxGroup options={drinks} value={selectedCheckbox} onChange={setSelectedCheckbox}/>
+
             </div>
             <div style={{ margin: '20px' }}>
                 <Button icon={<Icon.ChevronDown />}>qweqweqwe</Button>
+            </div>
+            <div style={{ margin: '20px' }}>
+                <InputList value={selectedCheckbox} action={<CheckboxGroup options={drinks} value={selectedCheckbox} onChange={setSelectedCheckbox} />} />
             </div>
         </>
     )
